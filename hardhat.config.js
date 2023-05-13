156f21331edf6fb9f6b15fccfbe0f4849576a9d4
Aliaksandra Abramovich <aliaksandra.abramovich@oliverwyman.com>
2023-05-13 22:08:52 +0200

    Add a script that withdraws from contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,26 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config()
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const SEPOLIA_URL = process.env.SEPOLIA_URL;
+const SEPOLIA_API_KEY = process.env.SEPOLIA_API_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_URL,
+      accounts: [PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: SEPOLIA_API_KEY,
+    }
+  }
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,21 +7,12 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const BuyMeACoffee = await hre.ethers.getContractFactory("BuyMeACoffee");
+  const buyMeACoffee = await BuyMeACoffee.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  await buyMeACoffee.deployed();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log("BuyMeACoffee deployed to:", buyMeACoffee.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
